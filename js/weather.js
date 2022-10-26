dd55fe1829f7bc2c31b5f4f3b33e12981287a928
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-26 11:32:00 +0900

    Created for weather api

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "2ffd467aa95aae62ea2190a89dec36cb";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Cant't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
